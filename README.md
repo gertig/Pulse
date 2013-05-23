fed5b297be1e568e0588954380114ab0dbae637c
AndrewGertig <andrew.gertig@gmail.com>
2013-05-23 12:29:28 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,70 @@
+## What is Uptime?
+
+Uptime is a Sinatra application that can be used to check to see if a website is currently up or down and then send you a Text message to alert of its change in status.
+
+## Getting Started
+
+Start application with:
+
+    $ git clone git://github.com/AndrewGertig/uptime.git
+    $ cd uptime
+    $ bundle
+    $ createdb yourappname
+    
+Using [Foreman, .env, and a Procfile](https://devcenter.heroku.com/articles/procfile#developing-locally-with-foreman)
+    
+    $ foreman start
+
+To access and change data via the console
+
+    $ irb
+    $ require './app.rb'
+    
+Create your first Site
+
+    $ Site.create(url: "http://andrewgertig.com")
+    
+Open your browser to whichever PORT you are using, (for me I have it set to PORT=5002 in my .env file so http://localhost:5002) and you should see something like this:
+
+![Heroku Scheduler](assets/site-demo.png)
+
+Green means the site is up, Red means it's down. I used to own two.io but couldn't ever figure out what to do with it :disappointed:
+    
+## Twilio Setup
+
+1. Get a Twilio SMS account
+2. Add your phone number and keys to your .env file
+3. Add your Twilio keys to Heroku config
+
+````
+$ heroku config:add MY_TWILIO_NUM=5558675309 TWILIO_ACCOUNT_SID=XXXXXXXXXSIDXXXXXXXX TWILIO_ACCOUNT_TOKEN=XXXXXXXXTOKENXXXXXXXX
+````
+    
+## Heroku Scheduler
+
+In order to check to see if a site is "UP" or "DOWN" you will need to setup a scheduled rake task using Heroku's Scheduler
+
+    $ heroku addons:add scheduler:standard
+    $ heroku addons:open scheduler
+    
+Create a Job that runs "rake check\_sites" every 10 minutes, it should look like this once you are done: 
+
+![Heroku Scheduler](assets/heroku-scheduler.png)  
+  
+## Datamapper Basics
+
+**Create**
+
+    $ Site.create(url: "http://andrewgertig.com")
+
+**Find**
+
+    $ Site.get(1)
+    $ Site.first(:url => "http://andrewgertig.com")
+
+**Destroy**
+
+    $ site = Site.get(5)
+    $ site.destroy  # => true
+
+####[Datamapper Documentation](http://datamapper.org/docs/)
---
+++ app.rb
@@ -0,0 +1,109 @@
+require 'sinatra'
+require 'data_mapper'
+require 'faraday'
+require 'twiliolib'
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/uptime')
+
+MY_CELL_PHONE = EVN["MY_CELL_PHONE"]
+MY_TWILIO_NUM = ENV["MY_TWILIO_NUM"]
+TWILIO_API_VERSION = "2010-04-01"
+TWILIO_ACCOUNT_SID = ENV["TWILIO_ACCOUNT_SID"]
+TWILIO_ACCOUNT_TOKEN = ENV["TWILIO_ACCOUNT_TOKEN"]
+
+class Site
+  include DataMapper::Resource
+  
+  property :id, Serial
+  property :url, String
+  property :last_check, DateTime
+  property :status_changed, DateTime
+  property :current_status, String, :default => "up"
+  property :notify, Boolean, :default => true
+  
+  validates_uniqueness_of :url
+  def down?
+    current_status == 'down'
+  end
+end
+
+class TwilioManager
+  def self.send_text(message)
+    return false if MY_TWILIO_NUM.nil?
+    
+    d = {
+        'From' => MY_TWILIO_NUM,
+        'To' => MY_CELL_PHONE,
+        'Body' =>  message
+    }
+    
+    begin
+        account = Twilio::RestAccount.new(TWILIO_ACCOUNT_SID, TWILIO_ACCOUNT_TOKEN)
+        resp = account.request(
+            "/#{TWILIO_API_VERSION}/Accounts/#{TWILIO_ACCOUNT_SID}/SMS/Messages",
+            'POST', d)
+        resp.error! unless resp.kind_of? Net::HTTPSuccess
+    rescue StandardError => bang
+        redirect_to({ :action => '.', 'msg' => "Error #{ bang }" })
+        return
+    end
+  end
+end
+
+# DataMapper.finalize.auto_upgrade!
+DataMapper.auto_upgrade!
+# Delayed::Worker.backend.auto_upgrade!
+
+get '/' do
+  erb :index
+end
+
+###################
+# METHODS
+###################
+def check_site(site)
+  site.last_check = Time.now
+    
+  status = result = nil
+  (1..3).each do |x|
+    begin
+      result = get_url(site.url)
+      status = is_down?(result) ? 'down' : 'up'
+      
+    rescue Faraday::Error::ConnectionFailed
+      puts "That is not a real site"
+      status = "down"
+    end
+      
+    break if status == 'up'
+    sleep(1)
+  end
+  
+  if site.current_status != status
+    site.current_status = status
+    site.status_changed = site.last_check
+    notify_change(site) if site.notify
+  end
+  raise "couldn't save" if !site.save
+end
+
+def get_url(uri)
+  conn = Faraday.new(:url => uri) do |faraday|
+    faraday.request  :url_encoded             # form-encode POST params
+    faraday.response :logger                  # log requests to STDOUT
+    faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+  end
+
+  response = conn.get #'/nigiri/sake.json'
+  response.status
+  # response.response.kind_of?(Net::HTTPSuccess)
+end
+
+def is_down?(result)
+  return false if result.to_i >= 200 && result.to_i < 400  #result.kind_of?(Net::HTTPSuccess) || result.kind_of?(Net::HTTPRedirection)
+  true
+end
+
+def notify_change(site)
+  TwilioManager.send_text("#{site.url} is #{site.current_status}")
+end
